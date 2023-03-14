88d79a62c83484552ec4024c4e999043c59603f7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-14 12:54:32 +0300

    adding grid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <Home/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,80 @@ import React from "react";
 import "./Home.css";
 export default function Home() {
   return (
-    <div>
-      <p>
-        <h1>Business Request</h1>
-        <h2>Create a DYNAMIC Map Chart With Drop-Downe</h2>
-      </p>
+    <div clssName="card-grid">
+      <div className="card">
+        <div classname="card-header">
+          Business Request for DYNAMIC Map Chart
+        </div>
+        <div className="card-body">
+          This will display the DYNAMIC Map chart businessRequst microsftword
+          documents
+        </div>
+        <div className="card-footer">
+          <a
+            className="App-link"
+            href="https://docs.google.com/spreadsheets/d/1ETQQwPaX8kMLgqIBLBulHTQiVk_nMf0rXOLxm4UnLko/edit#gid=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View the Dashboard Here
+          </a>
+        </div>
+      </div>
 
       <div className="card">
-        <div classname="card-header"> DYNAMIC Map Chart With Drop-Down</div>
+        <div classname="card-header">
+          {" "}
+          DYNAMIC Map Chart With Drop-Down Googlesheet Display
+        </div>
         <div className="card-body">
           This will display the DYNAMIC Map chart on googlesheet
         </div>
         <div className="card-footer">
-          <button className="btn btn-outline">
-            Click Here to Dynameic map on google sheeet
-          </button>
+          <a
+            className="App-link"
+            href="https://docs.google.com/spreadsheets/d/1ETQQwPaX8kMLgqIBLBulHTQiVk_nMf0rXOLxm4UnLko/edit#gid=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View the Dashboard Here
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div classname="card-header">
+          {" "}
+          DYNAMIC Map Chart With Drop-Down Excel File
+        </div>
+        <div className="card-body">This will provide the download file</div>
+        <div className="card-footer">
+          <a
+            className="App-link"
+            href="https://docs.google.com/spreadsheets/d/1ETQQwPaX8kMLgqIBLBulHTQiVk_nMf0rXOLxm4UnLko/edit#gid=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click here to download the excel file
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div classname="card-header">
+          {" "}
+          DYNAMIC Map Chart With Drop-Down MicrosoftPowerBI Published report
+        </div>
+        <div className="card-body">
+          This will provide the PDF published report
+        </div>
+        <div className="card-footer">
+          <a
+            className="App-link"
+            href="https://docs.google.com/spreadsheets/d/1ETQQwPaX8kMLgqIBLBulHTQiVk_nMf0rXOLxm4UnLko/edit#gid=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click here to download the pdf
+          </a>
         </div>
       </div>
     </div>
